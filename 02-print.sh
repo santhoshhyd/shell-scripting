c2a2c3723e631ee98274bfd0c1adbab89f7cb43c
Satish Kumar <gsatishkumar04@gmail.com>
2021-10-31 14:46:51 +0530

    code modified

--- 02-print.sh
+++ 02-print.sh
@@ -5,12 +5,14 @@ echo welcome to all
 echo -e "hello world\nwelcome to all"
 
 echo -e "word1\tword2"
+echo -e "word1\t\tword2"
 
 echo -e "\e[31mhello world"
 echo -e "\e[32mhello world"
 echo -e "\e[35mhello world"
-echo -e "hai to all"
+echo -e "\e[36mhello world\e[0m"
 
-echo -e "\e[0mwonderful day"
+
+
 
 
